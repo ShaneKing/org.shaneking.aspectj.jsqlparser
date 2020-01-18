b0772784616b2bc7a360dbd821ac8897e99f1b28
ShaneKing <github@azolla.org>
2020-01-18 22:21:55 +0800

    List2MapKeyGenerator

--- src/main/java/org/shaneking/aspectj/jsqlparser/annotation/SensitiveItemsFinderPath.java
+++ src/main/java/org/shaneking/aspectj/jsqlparser/annotation/SensitiveItemsFinderPath.java
@@ -1,5 +1,7 @@
 package org.shaneking.aspectj.jsqlparser.annotation;
 
+import org.shaneking.skava.lang.String0;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,5 +10,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface SensitiveItemsFinderPath {
-  String value() default "";
+  String value() default String0.EMPTY;
 }
